a264df168338c380476ab5a078637aad6b2f9524
akash2237778 <akash2237778@gmail.com>
2020-08-28 11:30:58 +0530

    branch: Jenkins_file commit : 1

--- jenkins_file/src/main/java/jenkins_file/App.java
+++ jenkins_file/src/main/java/jenkins_file/App.java
@@ -1,4 +1,4 @@
-package Jenkins_CICD;
+package jenkins_file;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello JenkinsFile" );
     }
 }
